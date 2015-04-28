e4a8bec4daf8bf6c2a18a36f77193370a6bd5e4a
sujinbaek <baeksj91@gmail.com>
2015-04-28 14:20:58 +0900

    2015-04-28 수진

--- app/src/main/java/Fragment/Userlist.java
+++ app/src/main/java/Fragment/Userlist.java
@@ -2,21 +2,103 @@ package Fragment;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.example.baek.baekkimchi.R;
+
+import java.util.ArrayList;
+
+import dataset.StoreData;
 
 
 public class Userlist extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<StoreData> asd;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_userlist, container, false);
+        View v = inflater.inflate(R.layout.fragment_ranklist, container, false);
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        asd = new ArrayList<StoreData>();
+
+        // specify an adapter
+        asd.add(new StoreData("A"));
+        asd.add(new StoreData("B"));
+        asd.add(new StoreData("C"));
+        asd.add(new StoreData("D"));
+
+        mAdapter = new StoreAdapter(asd);
+        mRecyclerView.setAdapter(mAdapter);
 
         return v;
     }
 
+    public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> {
+        private ArrayList<StoreData> mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v.findViewById(R.id.card_title_view);
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public StoreAdapter(ArrayList<StoreData> myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent,
+                                             int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_card, parent, false);
+//            v.setOnClickListener(getActivity().myOnClickListener);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            TextView tv = holder.mTextView;
+            tv.setText(mDataset.get(position).getText_contents());
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.size();
+        }
+    }
 }
+
