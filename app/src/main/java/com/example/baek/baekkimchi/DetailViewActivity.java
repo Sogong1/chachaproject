609ab678aedc2ad021d711df162ca8f35259ebb8
sujinbaek <baeksj91@gmail.com>
2015-06-09 20:42:53 +0900

    UI modified4

--- app/src/main/java/com/example/baek/baekkimchi/DetailViewActivity.java
+++ app/src/main/java/com/example/baek/baekkimchi/DetailViewActivity.java
@@ -1,12 +1,13 @@
 package com.example.baek.baekkimchi;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class DetailViewActivity extends ActionBarActivity {
+public class DetailViewActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
